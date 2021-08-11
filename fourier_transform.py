a9cf344b3e19a67cde659c2d00076610dcd6bbe3
Jean David <jeandavid54@aol.com>
2021-08-11 22:52:47 +0200

    Update fourier_transform.py

--- fourier_transform.py
+++ fourier_transform.py
@@ -8,13 +8,13 @@ sample_rate = 200
 # rotation clockwise
 exponent = -2*pi*1j
 
-
 def rot(cycle):
     return e**(exponent*cycle)
+
 # signal with n periods
-n = 2
+n = 5
 t = np.linspace(0,n,sample_rate)
-one_round = np.linspace(0,1,sample_rate)
+#n_round = np.linspace(0,n,sample_rate)
 
 # compound signal
 f1 = 2
@@ -24,14 +24,14 @@ s2 = (np.cos(2*pi*f2*t)+1)/2
 all_sig = (s1+s2)/2
 
 # signal to study
-one_period = ((np.cos(2*pi*f1*one_round)+1)/2 + (np.cos(2*pi*f2*one_round)+1)/2)/2
+# one_period = ((np.cos(2*pi*f1*n_round)+1)/2 + (np.cos(2*pi*f2*n_round)+1)/2)/2
 
 # spread one period of signal with as many rotations as cycles by period
-cycle = 2  
-cy = np.linspace(0,cycle,sample_rate)
+cycle = 2 
+cy = np.linspace(0, n*cycle, sample_rate)
 rotation = rot(cy)
 # do the rotation on one_period signal
-y = one_period*rotation
+y = all_sig*rotation
 
 fig, (ax1, ax2) = plt.subplots(1,2,figsize=(10,5))
 for k in range(1,n+1):
@@ -51,17 +51,18 @@ ax2.grid()
 plt.show()
 
 # cycling 0 to n
-n = 5
+fmax = 5
 fourier = []
-samples = 100
-cycles = np.arange(0, n+.1, n/samples)
-for c in cycles:
-    cy = np.linspace(0, c, sample_rate)
+samples = 500
+cycles = np.arange(0, fmax+.1, fmax/samples)
+for cycle in cycles:
+    cy = np.linspace(0,n*cycle, sample_rate)
     rotation = rot(cy)
-    y = one_period*rotation
-    s = sum(y)/sample_rate
+    y = all_sig*rotation
+    s = 2*sum(y)/sample_rate
     fourier.append(s.real)
-    print(c,s)
+    if int(cycle) == cycle:
+        print(cycle,s)
 plt.plot(cycles,fourier)
 plt.grid()
 plt.show()
